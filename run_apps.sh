db539d836dcb36f3d6913b2fb06a8d442e890abc
Rizky Andika Putra <rizky.andika@bukalapak.com>
2017-03-30 13:35:48 +0700

    6

--- run_apps.sh
+++ run_apps.sh
@@ -5,22 +5,22 @@ cd ~/jmeter/apache-jmeter-3.1/bin
 rm ~/workspace/ts_demo/pending_limit.jtl
 
 #user_profile
-./jmeter -n -t ~/workspace/ts_demo/Pending\ Limit.jmx -l ~/workspace/ts_demo/hasil.jtl
+./jmeter -n -t ~/workspace/ts_demo/Pending\ Limit.jmx -l ~/workspace/ts_demo/pending_limit.jtl
 
 #generate graph
 cd ~/jmeter/apache-jmeter-3.1/lib
 
 #responseTimesOverTime
-#java -jar cmdrunner-2.0.jar --tool Reporter --generate-png ~/workspace/ts_demo/responseTimesOverTime.png --input-jtl ~/workspace/ts_demo/pending_limit.jtl \--plugin-type ResponseTimesOverTime --width 800 --height 600
+java -jar cmdrunner-2.0.jar --tool Reporter --generate-png ~/workspace/ts_demo/responseTimesOverTime.png --input-jtl ~/workspace/ts_demo/pending_limit.jtl \--plugin-type ResponseTimesOverTime --width 800 --height 600
 
 #responseTimesDistribution
-#java -jar cmdrunner-2.0.jar --tool Reporter --generate-png ~/workspace/ts_demo/responseTimesDistribution.png --input-jtl ~/workspace/ts_demo/pending_limit.jtl \--plugin-type ResponseTimesDistribution --width 800 --height 600
+java -jar cmdrunner-2.0.jar --tool Reporter --generate-png ~/workspace/ts_demo/responseTimesDistribution.png --input-jtl ~/workspace/ts_demo/pending_limit.jtl \--plugin-type ResponseTimesDistribution --width 800 --height 600
 
 #responseTimesPercentiles
-#java -jar cmdrunner-2.0.jar --tool Reporter --generate-png ~/workspace/ts_demo/responseTimesPercentiles.png --input-jtl ~/workspace/ts_demo/pending_limit.jtl \--plugin-type ResponseTimesPercentiles --width 800 --height 600
+java -jar cmdrunner-2.0.jar --tool Reporter --generate-png ~/workspace/ts_demo/responseTimesPercentiles.png --input-jtl ~/workspace/ts_demo/pending_limit.jtl \--plugin-type ResponseTimesPercentiles --width 800 --height 600
 
 #synthesisReport
-#java -jar cmdrunner-2.0.jar --tool Reporter --generate-csv ~/workspace/ts_demo/synthesisReport_user_profile.csv --input-jtl ~/workspace/ts_demo/pending_limit.jtl \--plugin-type SynthesisReport  
+java -jar cmdrunner-2.0.jar --tool Reporter --generate-csv ~/workspace/ts_demo/synthesisReport_user_profile.csv --input-jtl ~/workspace/ts_demo/pending_limit.jtl \--plugin-type SynthesisReport  
 
 #summaryReport
-java -jar cmdrunner-2.0.jar --tool Reporter --generate-csv ~/workspace/ts_demo/aggregateResults.csv --input-jtl ~/workspace/ts_demo/hasil.jtl \--plugin-type AggregateReport
+#java -jar cmdrunner-2.0.jar --tool Reporter --generate-csv ~/workspace/ts_demo/aggregateResults.csv --input-jtl ~/workspace/ts_demo/hasil.jtl \--plugin-type AggregateReport
